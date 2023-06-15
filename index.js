f166a5d9d6dc5d98cdb488aeceae26621467e339
Sam <siacavazzi@gmail.com>
2023-06-15 13:51:35 -0400

    finished lab

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+
+
+
+function logTwoValues(value1, value2) {
+  console.log(`The two values are ${value1} and ${value2}.`);
+}
+
+function introductionWithLanguageOptional(name, language="JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
